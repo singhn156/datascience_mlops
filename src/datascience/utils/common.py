a002f39d76a296c4b4b0b36483be7bc969c1f460
Nikhil Singh <38158849+singhn156@users.noreply.github.com>
2026-06-14 20:39:28 +0000

    add common.py

---
+++ src/datascience/utils/common.py
@@ -0,0 +1,67 @@
+import os
+import yaml
+from src.datascience import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+from box.exceptions import BoxValueError
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path)-> ConfigBox:
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} load successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e    
+
+
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to
+    """
+
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+@ensure_annotations
+def save_json(path: Path,data:dict):
+    with open(path,"w") as f:
+        json.dump(data,f,indent=4)
+
+    logger.info(f"json file saved at: {path}")    
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    with open(path) as f:
+        content = json.load(f)
+
+    logger.info(f"json file load from: {path}")
+    return ConfigBox(content)
+
+
+@ensure_annotations
+def save_bin(path: Path,data:dict):
+    joblib.dump(value=data,filename=path)
+
+    logger.info(f"binary file saved at: {path}")    
+
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    data = joblib.load(path)
+    logger.info(f"binary file load from: {path}")
+    return data
